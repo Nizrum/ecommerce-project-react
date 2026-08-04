079428f80f08faf3feed5507499677f032302a1b
Semyon Murzin <62435919+Nizrum@users.noreply.github.com>
2026-08-04 13:49:49 +0400

    feat: change Links to NavLinks in Header in order to mark them as active

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router';
+import { NavLink } from 'react-router';
 import './Header.css';
 
 export function Header() {
@@ -6,7 +6,7 @@ export function Header() {
 		<>
 			<div className="header">
 				<div className="left-section">
-					<Link
+					<NavLink
 						to="/"
 						className="header-link">
 						<img
@@ -17,7 +17,7 @@ export function Header() {
 							className="mobile-logo"
 							src="images/mobile-logo-white.png"
 						/>
-					</Link>
+					</NavLink>
 				</div>
 
 				<div className="middle-section">
@@ -36,13 +36,13 @@ export function Header() {
 				</div>
 
 				<div className="right-section">
-					<Link
+					<NavLink
 						className="orders-link header-link"
 						to="/orders">
 						<span className="orders-text">Orders</span>
-					</Link>
+					</NavLink>
 
-					<Link
+					<NavLink
 						className="cart-link header-link"
 						to="/checkout">
 						<img
@@ -51,7 +51,7 @@ export function Header() {
 						/>
 						<div className="cart-quantity">3</div>
 						<div className="cart-text">Cart</div>
-					</Link>
+					</NavLink>
 				</div>
 			</div>
 		</>
