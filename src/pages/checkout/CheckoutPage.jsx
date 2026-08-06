ccd7a12a969c36d7d0e1ca47f875179cc6617001
Semyon Murzin <62435919+Nizrum@users.noreply.github.com>
2026-08-06 18:45:14 +0400

    refactor: convert home and checkout to async await

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -10,15 +10,15 @@ export function CheckoutPage({ cart }) {
 	const [paymentSummary, setPaymentSummary] = useState(null);
 
 	useEffect(() => {
-		axios
-			.get('/api/delivery-options?expand=estimatedDeliveryTime')
-			.then((response) => {
-				setDeliveryOptions(response.data);
-			});
+		const fetchCheckoutData = async () => {
+			const deliveryOptionsResponse = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime')
+			setDeliveryOptions(deliveryOptionsResponse.data);
 
-		axios.get('/api/payment-summary').then((response) => {
-			setPaymentSummary(response.data);
-		});
+			const paymentSummaryResponse = await axios.get('/api/payment-summary')
+			setPaymentSummary(paymentSummaryResponse.data);
+		};
+
+		fetchCheckoutData();
 	}, []);
 
 	return (
@@ -36,7 +36,10 @@ export function CheckoutPage({ cart }) {
 				<div className="page-title">Review your order</div>
 
 				<div className="checkout-grid">
-					<OrderSummary cart={cart} deliveryOptions={deliveryOptions} />
+					<OrderSummary
+						cart={cart}
+						deliveryOptions={deliveryOptions}
+					/>
 					<PaymentSummary paymentSummary={paymentSummary} />
 				</div>
 			</div>
