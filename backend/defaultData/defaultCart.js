c9559816cdc9eed60c44c2ea57a41b31e69b3256
Semyon Murzin <62435919+Nizrum@users.noreply.github.com>
2026-09-10 19:34:01 +0300

    style: improve styling

--- backend/defaultData/defaultCart.js
+++ backend/defaultData/defaultCart.js
@@ -4,9 +4,4 @@ export const defaultCart = [
     quantity: 2,
     deliveryOptionId: "1"
   },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-    deliveryOptionId: "2"
-  }
 ];
